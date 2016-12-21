0261080a7a4d4b01f1b10826ba7776eb68f09fd6
Daniele Palombo <ksemuldie@gmail.com>
2016-12-21 11:09:25 +0100

    Add helper to stub signature
    In test the signature can be stubbed using `stub_signature!`

--- spec/support/helpers/shipwire_hacks.rb
+++ spec/support/helpers/shipwire_hacks.rb
@@ -1,21 +1,32 @@
 module ShipwireHacks
-  def signature_hash(params)
-    { 'X-Shipwire-Signature' => signature(params.to_json) }
+  module StubbingHelpers
+    def stub_signature!
+      before do
+        allow(controller).to receive(:validate_key).and_return true
+      end
+    end
   end
 
-  private
+  module SignatureHelpers
+    def signature_hash(params)
+      { 'X-Shipwire-Signature' => signature(params.to_json) }
+    end
 
-  def signature(data)
-    Base64.encode64(
-      OpenSSL::HMAC.digest(
-        OpenSSL::Digest.new('sha256'),
-        Spree::SolidusShipwireConfig.secret,
-        data
-      )
-    ).strip
+    private
+
+    def signature(data)
+      Base64.encode64(
+        OpenSSL::HMAC.digest(
+          OpenSSL::Digest.new('sha256'),
+          Spree::SolidusShipwireConfig.secret,
+          data
+        )
+      ).strip
+    end
   end
 end
 
 RSpec.configure do |config|
-  config.include ShipwireHacks, type: :controller
+  config.extend  ShipwireHacks::StubbingHelpers,  type: :controller
+  config.include ShipwireHacks::SignatureHelpers, type: :controller
 end
