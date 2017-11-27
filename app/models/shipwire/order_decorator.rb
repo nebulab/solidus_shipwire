2ea78d676dea99ef198fc21c23919dfa2f6e559e
Daniele Palombo <daniele.palombo@gmail.com>
2017-11-27 17:33:39 +0100

    Waiting https://github.com/nebulab/shipwire/pull/2
    Revert this commit when the PR on solidus shipwire will be merged.

--- app/models/shipwire/order_decorator.rb
+++ app/models/shipwire/order_decorator.rb
@@ -1,10 +1,26 @@
 # Fix for retrieve order from Shipwire gem
 module SolidusShipwire
-  module FixFindOrder
+  module OrderDecorator
     def find(id, params = {})
       request(:get, "orders/#{id}", params: params)
     end
+
+    def holds_clear(id)
+      request(:post, "orders/#{id}/holds/clear")
+    end
+
+    def mark_processed(id)
+      request(:post, "orders/#{id}/markProcessed")
+    end
+
+    def mark_submitted(id)
+      request(:post, "orders/#{id}/markSubmitted")
+    end
+
+    def mark_complete(id)
+      request(:post, "orders/#{id}/markComplete")
+    end
+
+    Shipwire::Orders.prepend self
   end
 end
-
-Shipwire::Orders.prepend SolidusShipwire::FixFindOrder
