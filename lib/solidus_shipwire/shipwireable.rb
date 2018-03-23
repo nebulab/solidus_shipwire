8ce2c9918c5469b2638e48a4774882ae31096b22
Daniele Palombo <daniele.palombo@gmail.com>
2018-03-23 16:39:00 +0100

    Add acts_as_shipwireable_on method to share configuration
    acts_as_shipwireable_on sets config on shipwireable_config class
    attribute to share the configuration across the included module.

--- lib/solidus_shipwire/shipwireable.rb
+++ lib/solidus_shipwire/shipwireable.rb
@@ -1,6 +1,24 @@
 module SolidusShipwire
   module Shipwireable
+    def shipwireable?
+      false
+    end
+
     def acts_as_shipwireable(*args)
+      acts_as_shipwireable_on(*args)
+    end
+
+    private
+
+    def acts_as_shipwireable_on(config)
+      class_attribute :shipwireable_config unless shipwireable?
+      self.shipwireable_config = config
+
+      class_eval do
+        def self.shipwireable?
+          true
+        end
+      end
     end
   end
 end
--- spec/lib/solidus_shipwire/shipwireable_spec.rb
+++ spec/lib/solidus_shipwire/shipwireable_spec.rb
@@ -11,5 +11,21 @@ describe SolidusShipwire::Shipwireable do
     subject { dummy_class }
 
     it { is_expected.to respond_to :acts_as_shipwireable }
+
+    context ".acts_as_shipwireable" do
+      let(:shipwireable_config) { { config: :value } }
+      subject { dummy_class.acts_as_shipwireable(shipwireable_config) }
+
+      let(:dummy_class) do
+        Class.new do
+          extend SolidusShipwire::Shipwireable
+        end
+      end
+
+      it "sets shipwireable_config class variable" do
+        subject
+        expect(dummy_class.shipwireable_config).to eq shipwireable_config
+      end
+    end
   end
 end
