2b27950e74788da1490a32ab67dc89f8d651eaf9
Daniele Palombo <daniele.palombo@gmail.com>
2018-03-27 15:40:42 +0200

    Updated test classes to respond to find, update and create

--- spec/lib/solidus_shipwire/shipwireable/shipwire_api_spec.rb
+++ spec/lib/solidus_shipwire/shipwireable/shipwire_api_spec.rb
@@ -1,22 +1,23 @@
-class FakeShipwireApi < Shipwire::Api; end
+class FakeShipwireApi < Shipwire::Api
+  def find; end
 
+  def update; end
 
+  def create; end
+end
 
+class DummyClass
+  include ActiveModel::Model
+  extend SolidusShipwire::Shipwireable
 
+  acts_as_shipwireable api_class: FakeShipwireApi
+end
 
 describe SolidusShipwire::Shipwireable::ShipwireApi do
   context "when api_class is configured" do
-    let(:dummy_class) do
-      Class.new do
-        extend SolidusShipwire::Shipwireable
-
-        acts_as_shipwireable api_class: FakeShipwireApi
-      end
+    describe DummyClass do
+      it_behaves_like "shipwireable api class"
+      it_behaves_like "shipwireable api instance"
     end
-
-    let(:described_class) { dummy_class }
-
-    it_behaves_like "shipwire api shipwireable"
-    it_behaves_like "shipwire integrated object"
   end
 end
