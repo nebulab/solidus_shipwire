af4abb821aff249b0a9ff0a87eb0269defc33fd4
Simone Bravo <simonebravo96@gmail.com>
2018-03-28 17:01:51 +0200

    Improve module namespacing for ResponseDecorator

--- lib/solidus_shipwire/response_decorator.rb
+++ lib/solidus_shipwire/response_decorator.rb
@@ -1,19 +1,19 @@
-module SolidusShipwire::Response
-  def resource
-    body['resource'].with_indifferent_access
-  end
+module SolidusShipwire
+  module ResponseDecorator
+    def resource
+      body['resource'].with_indifferent_access
+    end
 
-  def to_sku_id_hashmap
-    Hash[resource[:items].map do |v|
-      [v[:resource][:sku], v[:resource][:id]]
-    end]
-  end
+    def to_sku_id_hashmap
+      Hash[resource[:items].map do |v|
+        [v[:resource][:sku], v[:resource][:id]]
+      end]
+    end
 
-  def next?
-    resource[:next].present?
-  end
+    def next?
+      resource[:next].present?
+    end
 
+    Shipwire::Response.prepend self
   end
 end
-
-Shipwire::Response.prepend SolidusShipwire::Response
