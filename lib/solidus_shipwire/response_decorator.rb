5ba2d33886589f49386292216fafab5917e7789d
Simone Bravo <simonebravo96@gmail.com>
2018-03-28 17:00:43 +0200

    Remove unused method parse_error_summary_from

--- lib/solidus_shipwire/response_decorator.rb
+++ lib/solidus_shipwire/response_decorator.rb
@@ -13,14 +13,6 @@ module SolidusShipwire::Response
     resource[:next].present?
   end
 
-  private
-
-  def parse_error_summary_from(body)
-    if (400..599).cover?(body['status']) && body.key?('message')
-      body['message']
-    else
-      body.fetch('error', nil).presence
-    end
   end
 end
 
